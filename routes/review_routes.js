522d57be7e2481f0b90cf5d3582b6def73f052bf
natakamm <natakamm1@googlemail.com>
2025-01-30 18:19:55 +0100

    added controllers and routes for review while updating the average ranking of each course. Not tested

--- routes/review_routes.js
+++ routes/review_routes.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const { getAllForOneCourse } = require("../controllers/review_controllers");
+const {
+  getAllForOneCourse,
+  createReview,
+  getYourReviews,
+  deleteReview,
+  updateReview,
+  getOneReview,
+  getAllReviews,
+} = require("../controllers/review_controllers");
+const requireAuth = require("../middlewares/requireAuth");
 const app = express.Router();
 
+app.get("/", requireAuth("admin"), getAllReviews);
+app.get("/:reviewId", getOneReview);
+app.get("/your_reviews", getYourReviews);
 app.get("/course/:id", getAllForOneCourse);
+
+app.post("/create", requireAuth("user"), createReview);
+app.put("/edit/:reviewId", updateReview);
+app.delete("/delete/:reviewId", deleteReview);
 
 module.exports = app;
